8d7ab8870ef258db09e00c043f8a0cee6111b4ae
Sarthak <218388722+Sarthak05-code@users.noreply.github.com>
2026-06-16 15:58:32 +0545

    refactor dining philosophers implementation for improved state management and synchronization

--- main.c
+++ main.c
@@ -1,91 +1,134 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
+#include <unistd.h>
 
-static const char SYS_CLEAR[] = "\033[H\033[2J";
+#define N 5
+#define MAX_CYCLES 5
+#define LEFT(id)  ((id + N - 1) % N)
+#define RIGHT(id) ((id + 1) % N)
 
-typedef struct
+typedef enum { THINKING, HUNGRY, EATING } state_t;
+
+state_t state[N];
+int meals_eaten[N] = {0};
+
+pthread_mutex_t mutex;          // protects shared state[] array
+pthread_cond_t cond[N];         // one condition variable per philosopher
+pthread_t philosophers[N];
+
+void print_state()
 {
-    char *name;
-    double hp;
-    char *buffer;
-    double *buff;
-    double *damage;
+    printf("\033[H\033[2J"); // clear screen for a live "dashboard" view
+    printf("Philosopher states:\n");
+    for (int i = 0; i < N; i++)
+    {
+        const char *s = state[i] == THINKING ? "THINKING" :
+                         state[i] == HUNGRY   ? "HUNGRY"   : "EATING";
+        printf("  P%d: %-8s (meals eaten: %d)\n", i, s, meals_eaten[i]);
+    }
+    printf("\n");
+}
 
-} Character_1;
-
-typedef struct
+// Check if philosopher i can start eating:
+// it must be HUNGRY and neither neighbor can currently be EATING.
+void test(int i)
 {
-    char *name;
-    double hp;
-    char *buffer;
-    double *heal;
-    double *damage;
+    if (state[i] == HUNGRY &&
+        state[LEFT(i)] != EATING &&
+        state[RIGHT(i)] != EATING)
+    {
+        state[i] = EATING;
+        print_state();
+        pthread_cond_signal(&cond[i]); // wake up philosopher i if it was waiting
+    }
+}
 
-} Character_2;
-
-typedef struct
+void think(int id)
 {
-    char *name;
-    double hp;
-    char *buffer;
-    double *buff;
-    double *damage;
+    sleep(rand() % 2 + 1);
+}
 
-} Character_3;
-
-typedef struct
+void eat(int id)
 {
-    char *name;
-    double hp;
-    char *buffer;
-    double *self_buff;
-    double *damage;
+    sleep(rand() % 2 + 1);
+}
 
-} Character_4;
+void take_forks(int id)
+{
+    pthread_mutex_lock(&mutex);
+    state[id] = HUNGRY;
+    print_state();
+    test(id); // see if we can eat immediately
+
+    // If we couldn't transition to EATING, wait until signaled by a neighbor
+    while (state[id] != EATING)
+    {
+        pthread_cond_wait(&cond[id], &mutex);
+    }
+    pthread_mutex_unlock(&mutex);
+}
+
+void put_forks(int id)
+{
+    pthread_mutex_lock(&mutex);
+    state[id] = THINKING;
+    meals_eaten[id]++;
+    print_state();
+
+    // Re-check neighbors: they may now be able to eat
+    test(LEFT(id));
+    test(RIGHT(id));
+    pthread_mutex_unlock(&mutex);
+}
+
+void *philosopher_routine(void *args)
+{
+    int id = *(int *)args;
+
+    for (int cycle = 0; cycle < MAX_CYCLES; cycle++)
+    {
+        think(id);
+        take_forks(id);
+        eat(id);
+        put_forks(id);
+    }
+    return NULL;
+}
 
 int main()
 {
-    printf("%s", SYS_CLEAR);
+    int ids[N];
 
-    double array[] = {4250.0, 6230.0, 3230.0, 2930.0};
-
-    double buff_val = 20.0;
-    double damage_val = 0.0;
-    double heal_val = 50.0;
-    double damage_val2 = 75.0;
-    double buff_val3 = 15.0;
-    double damage_val3 = 30.0;
-    double self_buff_val4 = 10.0;
-    double damage_val4 = 40.0;
-    Character_1 s1 = {"Cyrene", array[0], "True Damage", &buff_val, &damage_val};
-    Character_2 s2 = {"Blade", array[1], "Vulnerability", &heal_val, &damage_val2};
-    Character_3 s3 = {"Ashveil", array[2], "Follow-up attack buff", &buff_val3, &damage_val3};
-    Character_4 s4 = {"Acheron", array[3], "Def-shred", &self_buff_val4, &damage_val4};
-
-    printf("The name of the first character is : %s\n", s1.name);
-    printf("The name of the second character is : %s\n", s2.name);
-    printf("The name of the third character is : %s\n", s3.name);
-    printf("The name of the fourth character is : %s\n", s4.name);
-
-    double highest = s1.hp;
-
-    if (s2.hp > highest)
+    pthread_mutex_init(&mutex, NULL);
+    for (int i = 0; i < N; i++)
     {
-        highest = s2.hp;
-    }
-    else if (s3.hp > highest)
-    {
-        highest = s3.hp;
-    }
-    else if (s4.hp > highest)
-    {
-        highest = s4.hp;
-    }
-    else
-    {
-        // nothing as the s1.hp will be called the highest as default
+        state[i] = THINKING;
+        pthread_cond_init(&cond[i], NULL);
     }
 
-    printf("The highest hp is: %.2lf ", highest);
+    for (int i = 0; i < N; i++)
+    {
+        ids[i] = i;
+        pthread_create(&philosophers[i], NULL, philosopher_routine, &ids[i]);
+    }
+
+    for (int i = 0; i < N; i++)
+    {
+        pthread_join(philosophers[i], NULL);
+    }
+
+    printf("\nFinal meal counts:\n");
+    for (int i = 0; i < N; i++)
+    {
+        printf("  Philosopher %d ate %d times\n", i, meals_eaten[i]);
+    }
+
+    pthread_mutex_destroy(&mutex);
+    for (int i = 0; i < N; i++)
+    {
+        pthread_cond_destroy(&cond[i]);
+    }
 
     return 0;
 }
