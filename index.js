1f053cfa207731f6c68669bd456827a76ccbf871
Sarthak <218388722+Sarthak05-code@users.noreply.github.com>
2026-06-16 15:20:40 +0545

    add initial HTML, CSS, and JavaScript files for data fetching tutorial

---
+++ index.js
@@ -0,0 +1,19 @@
+const fetchData = async () => {
+    const Data = await fetch('https://dummyjson.com/products')
+    const res = await Data.json()
+    const products = res.products
+
+    const tbody = document.querySelector('#dataFetcher tbody')
+    for(let i = 0 ; i < products.length ; ++i) {
+        const row = document.createElement('tr')
+        row.innerHTML = `
+            <td>${products[i].id}</td>
+            <td>${products[i].title}</td>
+            <td>${products[i].price}</td>
+
+        `
+        tbody.appendChild(row)
+    }
+}
+
+fetchData()
