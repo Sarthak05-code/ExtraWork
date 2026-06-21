775811e0b6c753c99841dc9e7563a8542f798dec
Sarthak <218388722+Sarthak05-code@users.noreply.github.com>
2026-06-21 18:00:34 +0545

    more updates and fixes on the yml file and the tester for the yml.

--- Main.java
+++ Main.java
@@ -3,6 +3,7 @@ public class Main {
     public static void main(String[] args) {
         Caller caller  = new Caller();
         caller.runner();
+        caller.Text();
     }
 }
 
@@ -11,10 +12,18 @@ interface Name{
     public void runner();
 }
 
+abstract class Mainer {
+    public abstract void Text();
+}
 
-class Caller implements Name {
+class Caller extends Mainer implements Name {
     @Override
     public void runner() {
         System.out.println("This should work as intended. ");
+    }
+
+    @Override
+    public void Text() {
+        System.out.println("This should also work. ");
     }
 }
