4aaf2c53f436e820360eecfca194211ef041d80a
Sarthak <218388722+Sarthak05-code@users.noreply.github.com>
2026-06-23 17:37:30 +0545

    Update main.go

--- Go_WORKS/main.go
+++ Go_WORKS/main.go
@@ -3,12 +3,30 @@ package main
 import "fmt" 
 
 func main() {
-	var add = adder(1 , 2)
-	fmt.Printf("The sum is : %d", add)
+	var op  string
+	var a , b int
+
+	fmt.Println("Enter operation: ")
+	fmt.Scan(&op)
+
+	fmt.Println("Enter a and b: ")
+	fmt.Scan(&a , &b)
+
+	if op == "add" {
+		fmt.Print("The sum is : " , adder(a,b))
+	} else if op == "sub" {
+		fmt.Print("The subtraction is : ", subractor(a ,b))
+	} else {
+		fmt.Print("The answer was unkown. ",op)
+	}
 }
 
 
 func adder(a int , b int) int {
 	return a + b
+}
+
+func subractor(a int  , b int) int {
+	return  a - b
 }
 
