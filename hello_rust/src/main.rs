2cf44f1a89278698ec617b1b17532987e707f6da
Sarthak <218388722+Sarthak05-code@users.noreply.github.com>
2026-06-27 17:38:24 +0545

    Update main.rs

--- hello_rust/src/main.rs
+++ hello_rust/src/main.rs
@@ -1,19 +1,23 @@
-use std::io;
+trait Greet {
+    fn hello(&self) -> String;
+    fn goodbye(&self) -> String {
+        String::from("Bye")
+    }
+}
+struct Person {
+    name: String,
+}
 
+impl Greet for Person {
+    fn hello(&self) -> String {
+        format!("Hello , {}", self.name)
+    }
+}
 
 pub fn main() {
-    let value = "I love learning rust";
-    let mut string_value = String::new();
-    println!("Enter a word you wanna find: ");
-    io::stdin().read_line(&mut string_value).unwrap();
-    
-        
-    match value.find(string_value.trim()) {
-        Some(index) => {
-            println!("The value was found at : {}" , index);
-        }
-        None => {
-            println!("No such value exist. ")
-        }
-    }
+    let p = Person {
+        name: String::from("Sarthak"),
+    };
+    println!("{}", p.hello());
+    println!("{}", p.goodbye());
 }
