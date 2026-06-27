af8d5426be19084903fad3f375b69e73290cdf63
Sarthak <218388722+Sarthak05-code@users.noreply.github.com>
2026-06-27 16:53:14 +0545

    Update main.rs

--- hello_rust/src/main.rs
+++ hello_rust/src/main.rs
@@ -1,3 +1,19 @@
-fn main() {
-    println!("Hello, world!");
+use std::io;
+
+
+pub fn main() {
+    let value = "I love learning rust";
+    let mut string_value = String::new();
+    println!("Enter a word you wanna find: ");
+    io::stdin().read_line(&mut string_value).unwrap();
+    
+        
+    match value.find(string_value.trim()) {
+        Some(index) => {
+            println!("The value was found at : {}" , index);
+        }
+        None => {
+            println!("No such value exist. ")
+        }
+    }
 }
