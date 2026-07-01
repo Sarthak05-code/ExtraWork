13d6b8b52f047203d0a9f47cf2cac8c51092c612
Sarthak <218388722+Sarthak05-code@users.noreply.github.com>
2026-07-01 16:10:41 +0545

    matrix multiplication with go.

--- GoWork/main.go
+++ GoWork/main.go
@@ -1,19 +1,59 @@
 package main
 
-import (
-	"fmt"
-	"time"
-)
-
-func caller() {
-	time.Sleep(2000 * time.Millisecond)
-}
+import "fmt"
 
 func main() {
-	fmt.Println("Hello , ")
-	caller()
-	fmt.Print("...Sarthak")
 	
-	
+	var  m , n , o int
+	fmt.Println("Enter the row and colums of matrix 1: ")
+	fmt.Scanln(&m,&n)
+	fmt.Println("Enter the row for matrix 2 : ")
+	fmt.Scanln(&o)
+
+	matrix_one := make([][]int , m)
+	matrix_two := make([][]int , n)
+	for i:= 0 ; i < m ; i++ {
+		matrix_one[i] = make([]int , n)
+	}
+	for i := 0 ;i < n ; i++ {
+		matrix_two[i] = make([]int , o)
+	}
+
+	result := make([][]int , m)
+	for i:= 0 ; i < m ; i++ {
+		result[i] = make([]int , o)
+	}
+	fmt.Println("Enter value are matrix A: ")
+	for i := 0 ; i < m ; i++ {
+		for j := 0 ; j < n ; j++ {
+			fmt.Printf("A[%d][%d] : " , i , j)
+			fmt.Scan(&matrix_one[i][j])
+		}
+	}
+	fmt.Println("Enter value for matrix B: ")
+	for i := 0 ; i < n ; i++ {
+		for j := 0 ; j < o ; j++ {
+			fmt.Printf("B[%d][%d] : " , i , j)
+			fmt.Scan(&matrix_two[i][j])
+		}
+	}
+
+	fmt.Println("The matrix multiplication values are")
+	for i := 0 ; i < m ; i++ {
+		for j := 0 ; j < o ; j++ {
+			for k := 0 ; k < n ; k++ {
+				result[i][j] += matrix_one[i][k] * matrix_two[k][j]
+			}
+		}
+	}
+
+	for i := 0 ; i < m ; i++ {
+		for j := 0 ; j < o ; j++ {
+			fmt.Printf("%d " , result[i][j])
+		}
+		fmt.Println()
+	}
+
 	
 }
+
