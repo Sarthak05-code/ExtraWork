780bbc3ace2708a5bd06313857d43e96d3536526
Sarthak <218388722+Sarthak05-code@users.noreply.github.com>
2026-08-13 18:50:06 +0545

    Fun stuffs

--- main.py
+++ main.py
@@ -10,19 +10,18 @@ def main():
     array = [i for i in range(10) if i % 2 == 0]
     print(array)
     for i, arr in enumerate(array):
-        if arr % 2 == 0:
-            array[i] = arr + return_number()
+        array[i] = arr + return_number()
 
     array_2 = [i for i in range(10) if i % 2 != 0 and i * i == i * i]
 
     print(array)
     print(array_2)
 
-    for i, answer in enumerate(zip(array, array_2)):
-        if array[i] == array_2[i]:
-            print(f"\nThis value {array[i]} {array_2[i]} match")
+    for value1, value2 in enumerate(zip(array, array_2)):
+        if value1 == value2:
+            print(f"\nThis value {value1} {value2} match")
         else:
-            print(f"The value are {answer}")
+            print(f"The values are {value1}, {value2}")
 
 
 if __name__ == "__main__":
