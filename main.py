d8689ba2f5444728277593ef673f16f3111b648c
Sarthak <218388722+Sarthak05-code@users.noreply.github.com>
2026-08-13 18:47:32 +0545

    Enumerate and zip in python

--- main.py
+++ main.py
@@ -12,8 +12,17 @@ def main():
     for i, arr in enumerate(array):
         if arr % 2 == 0:
             array[i] = arr + return_number()
-    print("Hello, world")
+
+    array_2 = [i for i in range(10) if i % 2 != 0 and i * i == i * i]
+
     print(array)
+    print(array_2)
+
+    for i, answer in enumerate(zip(array, array_2)):
+        if array[i] == array_2[i]:
+            print(f"\nThis value {array[i]} {array_2[i]} match")
+        else:
+            print(f"The value are {answer}")
 
 
 if __name__ == "__main__":
