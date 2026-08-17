b77e56b4221fcf24da8df5877903c9bf4dbe12bc
Sarthak <218388722+Sarthak05-code@users.noreply.github.com>
2026-08-17 17:39:20 +0545

    Shit in a kettle bro

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import random as ran
+import time as t
 
 
 def return_number():
@@ -8,15 +9,19 @@ def return_number():
 
 def main():
     array = [i for i in range(10) if i % 2 == 0]
+    print("The even array values are : ")
     print(array)
+    t.sleep(2)
     for i, arr in enumerate(array):
         array[i] = arr + return_number()
 
     array_2 = [i for i in range(10) if i % 2 != 0 and i * i == i * i]
-
+    print("The new array value are:")
     print(array)
+    t.sleep(2)
+    print("The value of array 2 are: ")
     print(array_2)
-
+    t.sleep(2)
     for value1, value2 in zip(array, array_2):
         if value1 == value2:
             print(f"\nThis value {value1} {value2} match")
